9d85a5fde57ab0002d3854c214e6e04a73af34d5
jozek-hajduk_globant <jozek.hajduk@globant.com>
2024-09-10 14:12:44 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
