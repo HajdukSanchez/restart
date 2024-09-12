422a26289282d4e3a285f83e3592fe4cb1827d9a
jozek-hajduk_globant <jozek.hajduk@globant.com>
2024-09-12 14:33:13 -0500

    add animation for image and title

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -16,6 +16,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0 // Offset value in horizontal direction of button drag
     @State private var isAnimating: Bool = false // Indicator to trigger animation
+    @State private var imageOffset: CGSize = .zero // Offset value for animation for image
+    @State private var indicatorOpacity: Double = 1.0 // Opactity for the drag indicator we can handle with the image
+    @State private var textTile: String = "Share." // Title is going to change when image moves
     private var buttonSize: CGFloat = 80.0
     
     var body: some View {
@@ -25,10 +28,11 @@ struct OnboardingView: View {
             VStack(spacing: 20) {
                 Spacer()
                 VStack(spacing: 0) {
-                    Text("Share.")
+                    Text(textTile)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundStyle(Color.white)
+                        .transition(.opacity)
                     Text("""
                         It's now how much we give but
                         how much love we put into giving.
@@ -44,12 +48,49 @@ struct OnboardingView: View {
                 .animation(.easeOut(duration: 1), value: isAnimating)
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    // Left and Right drag limitation (using abs for both cases)
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        // If image moves, hide the indicator
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTile = "Give."
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    // If image stop moves, show the indicator
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTile = "Share."
+                                    }
+                                })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                 }
+                .overlay(alignment: .bottom, content: {
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundStyle(Color.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0) // Show first time
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity) // Show or hide each time image moves
+                })
                 Spacer()
                 ZStack {
                     Capsule()
